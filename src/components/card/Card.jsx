5fb414375a12c0e98553e58f23f938368b97533f
NickEsColR <nicoesol0818@gmail.com>
2024-02-18 15:52:05 -0500

    style: add styles to generic card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,11 +5,13 @@ import PropTypes from "prop-types";
  * This is a card component that receives two props: Image and Info.
  * @param {function} Image - This is a function that returns an image.
  * @param {function} Info - This is a function that returns the information of the card.
+ * @param {string} size - This is the size of the card. default is "sm". could be "sm", "md" or "lg".
  * @returns {JSX.Element}
  */
-export const Card = ({ Image, Info }) => {
+export const Card = ({ Image, Info, size }) => {
+
   return (
-    <div className="card">
+    <div className= {`card ${size}`}>
       <div className="image-container">
         <Image />
       </div>
@@ -23,4 +25,9 @@ export const Card = ({ Image, Info }) => {
 Card.propTypes = {
   Image: PropTypes.func.isRequired,
   Info: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(["sm", "md", "lg"])
+};
+
+Card.defaultProps = {
+  size: "sm"
 };
