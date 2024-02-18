1b61122e4a80b30171616acc88c33ef17e60402e
NickEsColR <nicoesol0818@gmail.com>
2024-02-18 15:55:38 -0500

    feat: create character list component

---
+++ src/feature/character/character-list/CharacterList.jsx
@@ -0,0 +1,11 @@
+import { CharacterItem } from "../character-item/CharacterItem";
+
+export const CharacterList = ({characters}) => {
+  return (
+    <div className="container-characters">
+      {characters.map((character) => (
+        <CharacterItem key={character.id} character={character} />
+      ))}
+    </div>
+  );
+};
