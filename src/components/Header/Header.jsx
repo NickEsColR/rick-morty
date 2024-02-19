4ed7f7308584b0e945c9292cad6a79e3cdfb21cd
NickEsColR <nicoesol0818@gmail.com>
2024-02-19 09:17:35 -0500

    sytyle: add style to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,11 @@ import iconRick from "../../assets/img/rick-sanchez-100.png";
 
 export const Header = () => {
   return (
-    <header className="App-header">
-      <img src={iconRick} alt="Logo" />
-      <h1 className="title">Rick & Morty</h1>
+    <header className="app-header">
+      <div className="logo-container">
+        <img src={iconRick} alt="Logo" />
+        <h1 className="title">Rick & Morty</h1>
+      </div>
       <div className="menu">
         <span className="menu-item">Characters</span>
         <span className="menu-item">Locations</span>
