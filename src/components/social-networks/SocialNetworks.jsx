cf1d7bcab11a80a73f9fe0cc00f16805f6df8e71
NickEsColR <nicoesol0818@gmail.com>
2024-02-19 10:10:28 -0500

    style: add style to social netwroks component

--- src/components/social-networks/SocialNetworks.jsx
+++ src/components/social-networks/SocialNetworks.jsx
@@ -1,14 +1,20 @@
 import "./SocialNetworks.css";
+import githubImage from "../../assets/icons8-github.svg";
+import linkedinImage from "../../assets/icons8-linkedin.svg";
 
 export const SocialNetworks = () => {
   return (
     <div className="social-networks-container">
       <h1>Contact me</h1>
-      <a href="https://github.com/NickEsColR" target="_blank">
-        <img src="../../assets/icons8-github.svg" alt="github" />
+      <a href="https://github.com/NickEsColR" target="_blank" rel="noreferrer">
+        <img src={githubImage} alt="github" />
       </a>
-      <a href="https://linkedin.com/in/nicolascolmenaressoftwareengineer/" target="_blank">
-        <img src="../../assets/icons8-linkedin.svg" alt="linkedin" />
+      <a
+        href="https://linkedin.com/in/nicolascolmenaressoftwareengineer/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={linkedinImage} alt="linkedin" />
       </a>
     </div>
   );
