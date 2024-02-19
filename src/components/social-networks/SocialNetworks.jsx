01252a5d2b61f21f4dbc2b6c0f66c3784f41480e
NickEsColR <nicoesol0818@gmail.com>
2024-02-19 09:54:45 -0500

    feat: create social network component

---
+++ src/components/social-networks/SocialNetworks.jsx
@@ -0,0 +1,15 @@
+import "./SocialNetworks.css";
+
+export const SocialNetworks = () => {
+  return (
+    <div className="social-networks-container">
+      <h1>Contact me</h1>
+      <a href="https://github.com/NickEsColR" target="_blank">
+        <img src="../../assets/icons8-github.svg" alt="github" />
+      </a>
+      <a href="https://linkedin.com/in/nicolascolmenaressoftwareengineer/" target="_blank">
+        <img src="../../assets/icons8-linkedin.svg" alt="linkedin" />
+      </a>
+    </div>
+  );
+};
