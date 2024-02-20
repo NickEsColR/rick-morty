22200b2d654478e3cd06f3c8bee689ca2c12fa29
NickEsColR <nicoesol0818@gmail.com>
2024-02-19 21:36:49 -0500

    feat: add pagination to characters

--- src/Pages/Characters/Characters.jsx
+++ src/Pages/Characters/Characters.jsx
@@ -1,6 +1,7 @@
 import "./Characters.css";
 import { CharacterList } from "../../feature/character/components/character-list/CharacterList";
 import { CharacterContextProvider } from "../../feature/character/contexts/CharacterContext";
+import { Pagination } from "../../components/pagination/Pagination";
 
 export const Characters = () => {
   return (
@@ -8,6 +9,7 @@ export const Characters = () => {
       <div className="characters">
         <h1>Characters</h1>
         <CharacterList />
+        <Pagination />
       </div>
     </CharacterContextProvider>
   );
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -5,7 +5,7 @@ export const Pagination = () => {
     <nav className='pagination' role='navigation' aria-label='pagination'>
       <span className='pagination-btn'>&lt;</span>
       <ul className='pagination-list'>
-        <li><span className='pagination-btn'>1</span></li>
+        <li><span className='pagination-btn active'>1</span></li>
         <li><span className='pagination-btn'>2</span></li>
         <li><span className='pagination-btn'>3</span></li>
         <li><span className='pagination-btn'>4</span></li>
