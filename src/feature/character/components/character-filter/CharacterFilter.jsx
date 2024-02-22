9fd09a259c2505b14ddbf7b499bc9218afc06204
NickEsColR <nicoesol0818@gmail.com>
2024-02-22 08:51:31 -0500

    feat: create character filter constants

--- src/feature/character/components/character-filter/CharacterFilter.jsx
+++ src/feature/character/components/character-filter/CharacterFilter.jsx
@@ -1,15 +1,17 @@
 import { DropdownMenu } from '../../../../components/dropdown-menu/DropdownMenu'
 import { SearchBar } from '../../../../components/search-bar/SearchBar'
+import { GENDER, STATUS } from '../../constants/CharacterFilterOptions'
 import './CharacterFilter.css'
 
-export const CharacterFilter = () => {
+export const CharacterFilter = ({children}) => {
   return (
     <div className='filter-container'>
       <SearchBar labelText='name' />
-      <DropdownMenu labelText='status' options={['Alive', 'Dead', 'unknown']} />
+      <DropdownMenu labelText='status' options={STATUS} />
       <SearchBar labelText='species' />
-      <DropdownMenu labelText='gender' options={['female','male','genderless','unknown']} />
+      <DropdownMenu labelText='gender' options={GENDER} />
       <SearchBar labelText='type' />
+      {children}
     </div>
   )
 }
