6a2312acfbfeba1d85da6cb663ee50849a2f5ef7
NickEsColR <nicoesol0818@gmail.com>
2024-02-23 07:11:29 -0500

    feat: add filter error state to character context

--- src/feature/character/contexts/CharacterContext.jsx
+++ src/feature/character/contexts/CharacterContext.jsx
@@ -1,19 +1,29 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 
 export const CharacterContext = createContext([null, () => {}, true, () => {}]);
 
 /**
  * This function is a provider for the CharacterContext
- * @param {JSX.Element} children - elements that are wrapped by the provider 
+ * @param {JSX.Element} children - elements that are wrapped by the provider
  * @returns {JSX.Element}
  */
 export const CharacterContextProvider = ({ children }) => {
   const [characters, setCharacters] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [hasFilterError, setHasFilterError] = useState(false);
 
   return (
-    <CharacterContext.Provider value={[characters, setCharacters, loading, setLoading]}>
+    <CharacterContext.Provider
+      value={[
+        characters,
+        setCharacters,
+        loading,
+        setLoading,
+        hasFilterError,
+        setHasFilterError,
+      ]}
+    >
       {children}
     </CharacterContext.Provider>
-  )
+  );
 };
