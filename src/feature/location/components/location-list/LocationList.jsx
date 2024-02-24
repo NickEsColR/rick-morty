4f705844b8ebc5bc1835fe4a122e68f86e795f10
NickEsColR <nicoesol0818@gmail.com>
2024-02-23 20:54:51 -0500

    feat: create location list component

---
+++ src/feature/location/components/location-list/LocationList.jsx
@@ -0,0 +1,43 @@
+import "./LocationList.css";
+import { LocationItem } from "../location-item/LocationItem";
+import { useLocation } from "../../hooks/useLocation";
+import { Pagination } from "../../../../components/pagination/Pagination";
+import { LOCATIONS } from "../../../../Constants/ResourcesAPI";
+import { FilterError } from "../../../../components/filter-error/FilterError";
+import { useList } from "../../../../hooks/useList";
+import { useLocationFilter } from "../../hooks/useLocationFilter";
+
+/**
+ * Component that renders the list of Locations
+ * @returns {JSX.Element}
+ */
+export const LocationList = () => {
+  const { locations, setLocations } = useLocation();
+
+  const { loading, hasFilterError } = useList();
+
+  const { getFilter } = useLocationFilter();
+
+  if (hasFilterError) {
+    return <FilterError />;
+  }
+
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="container-locations">
+          {locations.map((location) => (
+            <LocationItem key={location.id} location={location} />
+          ))}
+        </div>
+      )}
+      <Pagination
+        resource={LOCATIONS}
+        setItems={setLocations}
+        getFilter={getFilter}
+      />
+    </>
+  );
+};
