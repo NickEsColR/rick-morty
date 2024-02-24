3e6551512c273c4a86484471442cb4004bfbf65f
NickEsColR <nicoesol0818@gmail.com>
2024-02-23 20:59:38 -0500

    feat: create location filter component

---
+++ src/feature/location/components/location-filter/LocationFilter.jsx
@@ -0,0 +1,42 @@
+import { SearchBar } from "../../../../components/search-bar/SearchBar";
+import { useLocationFilter } from "../../hooks/useLocationFilter";
+import "./LocationFilter.css";
+
+/**
+ * Component that renders the Location filter
+ * @param {Object} children - Elemnts inside the component
+ * @returns {JSX.Element}
+ */
+export const LocationFilter = ({ children }) => {
+  const {
+    name,
+    type,
+    dimension,
+    onChangeName,
+    onChangeType,
+    onChangeDimension,
+    filter,
+    clear,
+  } = useLocationFilter();
+
+  return (
+    <>
+      <div className="filter-container">
+        <div className="search-bars-container">
+          <SearchBar labelText="Name" value={name} onChange={onChangeName} />
+          <SearchBar labelText="Type" value={type} onChange={onChangeType} />
+          <SearchBar
+            labelText="Dimension"
+            value={dimension}
+            onChange={onChangeDimension}
+          />
+        </div>
+        <div className="btn-container">
+          <button onClick={filter}>Filter</button>
+          <button onClick={clear}>Clear</button>
+        </div>
+      </div>
+      {children}
+    </>
+  );
+};
