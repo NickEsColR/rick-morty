ea934f66b70a661cc876eb1b95a928296042586c
NickEsColR <nicoesol0818@gmail.com>
2024-02-23 21:07:04 -0500

    feat: add location page render

--- src/Pages/locations/Locations.jsx
+++ src/Pages/locations/Locations.jsx
@@ -1,7 +1,30 @@
-import './Locations.css'
+import { ListContextProvider } from "../../contexts/ListContext";
+import { PaginationContextProvider } from "../../contexts/PaginationContext";
+import { LocationFilter } from "../../feature/location/components/location-filter/LocationFilter";
+import { LocationList } from "../../feature/location/components/location-list/LocationList";
+import { LocationContextProvider } from "../../feature/location/contexts/LocationContext";
+import { LocationFilterContextProvider } from "../../feature/location/contexts/LocationFilterContext";
+import "./Locations.css";
 
+/**
+ * Component that renders Locations page
+ * @returns {JSX.Element}
+ */
 export const Locations = () => {
   return (
-    <div>Locations</div>
-  )
-}
+    <ListContextProvider>
+      <PaginationContextProvider>
+        <LocationContextProvider>
+          <LocationFilterContextProvider>
+            <div className="locations">
+              <h1 className="title">Locations</h1>
+              <LocationFilter>
+                <LocationList />
+              </LocationFilter>
+            </div>
+          </LocationFilterContextProvider>
+        </LocationContextProvider>
+      </PaginationContextProvider>
+    </ListContextProvider>
+  );
+};
